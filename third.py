c94d39bcdefdb5f6348832bb48d7622f4951b98a
waffleprinter <112639491+waffleprinter@users.noreply.github.com>
2022-09-29 16:01:05 -0400

    Update third.py

--- third.py
+++ third.py
@@ -1,6 +1,28 @@
 import random
 
 def encounter_monster():
+  global monster_strength
   monster_strength = random.randint(1, 5)
   print(f"Vous tombez face à face avec un adversaire de difficulté {monster_strength}.")
-  action == int(input(
+  
+  action == int(input("""
+  Que voulez-vous faire?
+      1- Combattre cet adversaire
+      2- Contourner cet adversaire et aller ouvrir une autre porte
+      3- Afficher les règles du jeu
+      4- Quitter la partie"""))
+
+
+def fight_monster():
+  global current_monster, monster_strength, health, battles_fought, battles_won, battles_lost
+  
+  print(f"""
+  Adversaire: {current_monster}.
+  Force de l'adversaire: {monster_strength}.
+  Niveau de vie de l'usager: {health}.
+  Combat {battles_fought}: {battles_won} victoires vs {battles_lost} défaites.""")
+  
+  player_roll = random.randint(1, 6)
+  
+  print(f"Lancé du dé: {player_roll}.")
+  
