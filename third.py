ead9a897835bb4e722f834f60ecf0d5986396db6
waffleprinter <112639491+waffleprinter@users.noreply.github.com>
2022-09-29 13:41:53 -0400

    Create third.py

---
+++ third.py
@@ -0,0 +1,6 @@
+import random
+
+def encounter_monster():
+  monster_strength = random.randint(1, 5)
+  print(f"Vous tombez face à face avec un adversaire de difficulté {monster_strength}.")
+  action == int(input(
