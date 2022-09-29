de7ee195ce6fad8fd856f0019a99d8e2ae7062a7
waffleprinter <112639491+waffleprinter@users.noreply.github.com>
2022-09-29 09:18:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import random
 
+i = 0
+monster_list = []
 
 class Player:
     health = 20
     strength = random.randint(1, 6)
-    fights_won = 0 
+    fights_won = 0
     fight_lost = 0
 
 
 class Monster:
-    def __init__(self, strength):
-        self.strength = random.randint(1, 5)
+    def __init__(self, strength=None):
+        self.strength = strength if strength else random.randint(1, 6)
 
 
+def create_monster():
+    global i
+    i += 1
+    monster_list.append(Monster(f"m{i}"))
 
 
+def list_choices():
+    create_monster()
+    print(f"Vous tombez en face avec un adversaire de difficulté: {}")
+
+list_choices()
