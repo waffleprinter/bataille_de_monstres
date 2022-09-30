7e8b9bcafd76e02a3fd7507f1b7636da92c3ec3b
waffleprinter <112639491+waffleprinter@users.noreply.github.com>
2022-09-30 15:31:28 -0400

    Added the ability for the player to die

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ class Player:
     health = 20
     battles_won = 0
     battles_lost = 0
-        
+    
+    def reset():
+        health = 20
+        battles_won = 0
+        battles_lost = 0
 
 class Monster:
     def __init__(self, strength=None):
@@ -17,7 +21,6 @@ class Monster:
 
 def encounter_monster():
     print(f"Vous tombez face à face avec un adversaire de difficulté {monster.strength}")
-
     action = int(input("Que voulez vous faire?:\n"
                        "  1. Combattre cet adversaire.\n"
                        "  2. Contourner cet adversaire et aller ouvrir une autre porte.\n"
@@ -26,23 +29,26 @@ def encounter_monster():
 
     if action == 1:
         print("You fight the monster!")
-
+        
     elif action == 2:
         run_away()
         encounter_monster()
-
+        
     elif action == 3:
         list_rules()
         encounter_monster()
-
+        
     else:
         quit_game()
 
 
 def run_away():
     player.health -= 1
-    monster.reset()
-    print(f"Niveau de vie: {player.health}\n")
+    if player.health < 1:
+        reset_game()
+    else:
+        monster.reset()
+        print(f"Niveau de vie: {player.health}\n")
 
 def list_rules():
     print("Pour réussir un combat, il faut que la valeur du dé lancé soit supérieure à la force de l’adversaire.\n"
@@ -56,6 +62,13 @@ def list_rules():
 
 def quit_game():
     print("Merci et au revoir...")
+    
+
+def reset_game():
+    print(f"La partie est terminée, vous avez vaincu {battles_won} monstres.")
+    player.reset()
+    monster.reset()
+    
 
 
 player = Player()
